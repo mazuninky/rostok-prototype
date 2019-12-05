fedb6134da7ae03769cf45aa7d8d1fb819a43a3f
Mazunin Konstantin <mazuninky@gmail.com>
2019-12-05 14:59:26 +0300

    Added placemark

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Row, Col} from 'antd';
 import './App.css';
-import {YMaps, Map} from 'react-yandex-maps';
+import {YMaps, Map, Placemark} from 'react-yandex-maps';
 import {
     BrowserRouter as Router,
     Switch,
@@ -11,14 +11,24 @@ import {
 import Clubs from './pages/clubs/index'
 import Event from './pages/event/index'
 
+import data from './data';
+
 function App() {
+    const marks = data.clubs.map(it => {
+        return (
+            <Placemark key={it.name} geometry={[it.lat, it.lon]} />
+        );
+    });
+
     return (
         <div className="App">
             <Row className="root-row">
                 <Col span={12}>
                     <YMaps>
-                        <Map className="map" defaultState={{center: [55.75, 37.57], zoom: 9}}
-                             defaultOptions={{autoFitToViewport: "always"}}/>
+                        <Map className="map" defaultState={{center: [59.93863, 30.31413], zoom: 9}}
+                             defaultOptions={{autoFitToViewport: "always"}}>
+                            {marks}
+                        </Map>
                     </YMaps>
                 </Col>
                 <Col span={12}>
